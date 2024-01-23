818937c9c83788667a7bb030a4f2597e66799bae
Runador <runador123456@gmail.com>
2024-01-23 23:15:16 +0200

    Adding a continue of the game functionality

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -11,26 +11,67 @@ public class RockPaperScissors {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Choose [r]ock, [p]aper or [s]cissors: ");
-        String playerMove = scanner.nextLine();
+        while (true) {
 
-        if (playerMove.equals("r") || playerMove.equals("rock")) {
-            playerMove = ROCK;
-        } else if (playerMove.equals("p") || playerMove.equals("paper")) {
-            playerMove = PAPER;
-        } else if (playerMove.equals("s") || playerMove.equals("scissors")) {
-            playerMove = SCISSORS;
-        } else {
-            System.out.println("Invalid Input. Try again...");
-            return;
+            System.out.print("\nChoose [r]ock, [p]aper or [s]cissors: ");
+            String playerMove = scanner.nextLine();
+
+            if (playerMove.equals("r") || playerMove.equals("rock")) {
+                playerMove = ROCK;
+            } else if (playerMove.equals("p") || playerMove.equals("paper")) {
+                playerMove = PAPER;
+            } else if (playerMove.equals("s") || playerMove.equals("scissors")) {
+                playerMove = SCISSORS;
+            } else {
+                System.out.println("Invalid Input. Try again...");
+                return;
+            }
+
+            Random random = new Random();
+            int computerRandomNumber = random.nextInt(4);
+
+            String computerMove = "";
+
+            switch (computerRandomNumber) {
+                case 0:
+                    computerMove = ROCK;
+                    break;
+                case 1:
+                    computerMove = PAPER;
+                    break;
+                case 2:
+                    computerMove = SCISSORS;
+                    break;
+                case 3:
+                    computerMove = ROCK;
+                    break;
+            }
+
+            System.out.printf("%nThe computer choose %s%n", computerMove);
+
+            if ((playerMove.equals(ROCK) && computerMove.equals(SCISSORS)) ||
+                    (playerMove.equals(PAPER) && computerMove.equals(ROCK)) ||
+                    (playerMove.equals(SCISSORS) && computerMove.equals(PAPER))) {
+                System.out.println("You win.");
+            } else if ((playerMove.equals(ROCK) && computerMove.equals(PAPER)) ||
+                    (playerMove.equals(PAPER) && computerMove.equals(SCISSORS)) ||
+                    (playerMove.equals(SCISSORS) && computerMove.equals(ROCK))) {
+                System.out.println("You lose.");
+            } else {
+                System.out.println("This game was a draw.");
+            }
+
+
+            System.out.println("\nDo you want to continue? [Yes] or [No] ?..");
+            String yesOrNo = scanner.nextLine();
+
+            if (yesOrNo.equals("Yes")) {
+                continue;
+            } else {
+                System.out.println("\nGame over.");
+                break;
+            }
         }
-
-        Random random = new Random();
-        int computerRandomNumber = random.nextInt(4);
-
-        String computerMove = "";
-
-
 
     }
 }
